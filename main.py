561c06a878eb544faa0916f347fdca4d30982fc9
buchimo <kkamibuchimoko81g@gmail.com>
2023-05-08 15:01:16 +0800

    submit

--- main.py
+++ main.py
@@ -5,11 +5,51 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/submit_pfb', methods=['POST'])
+def submit_pfb():
+    """ Create transaction
+    """
+    try:
+        data = request.get_json(force=True)
+        node_url = 'http://127.0.0.1:26659'
+
+        if (
+            not data['message'] 
+        ):
+           raise Exception('Insufficient data')
+
+        random_bytes = os.urandom(8)
+        namespace_id = random_bytes.hex()
+        data_hex = data['message'].encode('utf-8').hex()
+
+        tx_submit = requests.post(
+            node_url + '/submit_pfb',
+            json={'namespace_id': namespace_id, 'data': data_hex, 'gas_limit': 80000, "fee": 2000}
+        )
+        tx_submit_data = tx_submit.json()
+
+        print(tx_submit_data)
+
+        if not tx_submit_data['txhash']: 
+            raise Exception('Transaction decline')
+
+        txhash = tx_submit_data['txhash']
+        height = tx_submit_data['height']
+
+        return jsonify({
+                'txhash': txhash,
+                'namespace_id': namespace_id,
+                'height': height
+               })
+
+    except Exception as e:
+        return jsonify({'error': 500, 'message': str(e)}), 500
+
 @app.route('/')
 def index():
     """ Home page
     """
-    return render_template('index.html', gas_limit=80000, fee=2000, node_url='http://127.0.0.1:26659')
+    return render_template('index.html',  node_url='http://127.0.0.1:26659')
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,44 @@
 {
-    
+    const api = {
+        submit_pfb: "/submit_pfb"
+    };
+
+    const form = document.body.querySelector("form");
+
+    // create transaction
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        document.getElementById("tx_result").innerHTML = "executing...";
+
+        let tx_form = {
+            "message": form['message'].value
+        };
+
+        console.log(tx_form)
+
+        const connect = await fetch(api.submit_pfb, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(tx_form)
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Data error');
+        }).then(data => {
+            document.getElementById("tx_result").innerHTML = "<h3 style='color:green'>Submit Success</h3>";
+            
+            console.log(data)
+            
+            // document.getElementById("tx_info").hidden = false
+            // document.getElementById("tx_info_tx_hash").innerHTML = "<a target='_blank' href='https://testnet.mintscan.io/celestia-incentivized-testnet/txs/" + data.txhash + "?height=" + data.height + "'>" + data.txhash + "</a>";
+            // document.getElementById("tx_info_height").innerHTML = data.height;
+
+            return data;
+        }).catch(err => {
+            document.getElementById("tx_result").innerHTML = "<h3 style='color:red'>Failed, Please make sure the celestia node is available and try again!</h3>";
+        });
+    });
 }
